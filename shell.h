51a12eba047c8c2fff83c2110001b26a0cf77ff3
yaniG <yane12girma@gmail.com>
2023-10-19 02:51:40 -0400

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,51 +1,52 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/resource.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include "./stringfile/string.h"
-#include "linklist.h"
-#include "./memofile/memo.h"
-#include "shellvar.h"
-#include "./builtinfile/builtin.h"
-#include "./errorfile/error.h"
-#include "./shellhelperfile/shellhelper.h"
-#include "log.h"
-#define SPACE " "
-#define EQUAL "="
-#define COLON ":"
-#define BSLASH "\\"
-#define FSLASH "/"
-#define BUF_SIZE 4096
-#define PS1 write(STDOUT_FILENO, "MANDALORIAN$ ", 4)
-#define PS2 write(STDOUT_FILENO, "\nMANDALORIAN$ ", 5)
-#define NEWLINE write(STDOUT_FILENO, "\n", 1)
-#define DELIM " \t"
-/* _getline.c */
-ssize_t _getline(char **lineptr, size_t *n, int fd, list_t **mt);
-void reset_buffer(char *buffer, ssize_t buf_size);
+#include <errno.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFSIZE 100 /* constant buffer to use in toekn functions*/
+#define DELIM " \t\n" /* delimiters to check */
+
+/**
+ * struct choose_builtin - Builtin commands struct
+ * @name_builtin: Name of builtin command
+ * @func_builtin: Pointer to builtin command functions
+ */
+typedef struct choose_builtin
+{
+	char *name_builtin;
+	int (*func_builtin)(char **, char *, int *);
+} choose_builtins_t;
+
+/* global variable to access the enviromment list */
+extern char **environ;
+
+/* execute commands */
+int hsh_execute(char *args[], char *argv[], int *exit_status);
+int hsh_execute_builtins(char *args[], char *input_stdin,
+		char *argv[], int *exit_status);
+
+/* validate inputs */
+char *validate_input(char *args[], char *argv[] __attribute__((unused)));
+int validate_only_spaces(char *input);
+
+/* tokenizers */
+char **hsh_tokenizer(char *input);
+char **tokenizer_path(char *input);
+
+/* builtin functions */
+int hsh_cd(char *args[], char *input_stdin, int *exit_status);
+int hsh_setenv(char *args[],  char *input_stdin, int *exit_status);
+int hsh_unsetenv(char **args,  char *input_stdin, int *exit_status);
+int hsh_env(char **args, char *input_stdin, int *exit_status);
+int hsh_exit(char **args, char *input_stdin, int *exit_status);
+
+/* helper functions*/
+char *str_concat(char *s1, char *s2);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_strdup(char *str, list_t **mt);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strtok(char *s, const char *delim);
-unsigned int _strlen(char *s);
-/* string02.c */
-int _strcmp(char *s1, char *s2);
-int _atoi(char *s);
-char *_strcat(char *s1, char *s2, list_t **mt);
-char *_itoa(ssize_t num, list_t **mt);
-char *reverse_str(char *s);
-/* string03.c */
-char *_strchr(char *s, char c);
-void _chartostr(char *s, char c);
-#endif /* _SHELL_H_ */
+
+#endif
